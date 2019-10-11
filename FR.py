e461b5c9fb810cbbfcd3a628e686cbd6d436983e
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-10-11 14:02:21 +0200

    ;)

--- FR.py
+++ FR.py
@@ -2,15 +2,11 @@ import cv2
 import os
 import glob
 import numpy as np
-import pandas as pd
-from PIL import Image
 
 img_dir = "D:\PR1\Face-Recognition\ATT" # Enter Directory of all images 
 data_path = os.path.join(img_dir,'*g')
 files = glob.glob(data_path)
 data = []
-
-print("Images Loaded")
 i=0
 for f1 in files:
     i=i+1
@@ -19,8 +15,6 @@ for f1 in files:
     data.append(gray)
     mydata = np.array(data)
     print("Image : \n",mydata)
-
-       
 ##fixing shapev->400*10304
 d=np.reshape(mydata, (400, 10304))
 print("Shape od D matrix :",d.shape)
