843d9fa21efd53fdb624c497286434e1b8a71def
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-10-12 19:54:32 +0200

    Add Label NOT Fixed

--- FR.py
+++ FR.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import glob
 import numpy as np
+import pandas as pd
 
 img_dir = "D:\PR1\Face-Recognition\ATT" # Enter Directory of all images 
 data_path = os.path.join(img_dir,'*g')
@@ -18,3 +19,21 @@ for f1 in files:
 ##fixing shapev->400*10304
 d=np.reshape(mydata, (400, 10304))
 print("Shape od D matrix :",d.shape)
+
+np.savetxt("bigD.csv", d, delimiter=",")
+#prepare the Labels
+nameOflabels = []
+
+for i in range(0, 10304):
+    personN=1
+    person= str(personN)
+    nameOflabels.append(person)
+    if (personN%40)<1:
+            personN = personN +1
+    
+
+d = pd.read_csv("bigD.csv", sep=',')
+    
+xx = d.iloc[:, 0:10304]  # independent columns
+xx.columns =nameOflabels
+print(xx)
